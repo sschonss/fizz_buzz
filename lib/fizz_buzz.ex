5d10351c5c133f2ca48f194c9800cb2269f959e4
sschonss <schonsluuiz@gmail.com>
2023-07-11 21:04:46 -0300

    Flow Control with Pattern Matching

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,10 +1,11 @@
 defmodule FizzBuzz do
 
   def build(file_name) do
-    case File.read(file_name) do
-      {:ok, content} -> content
-      {:error, reason} -> reason
-    end
+    file = File.read(file_name)
+    handle_file_read(file)
   end
+
+  def handle_file_read({:ok, content}), do: content
+  def handle_file_read({:error, reason}), do: reason
 
 end
