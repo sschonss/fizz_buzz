2d90567a2198064f4dc046e6d9212a3742cdb513
sschonss <schonsluuiz@gmail.com>
2023-07-11 21:12:59 -0300

    String to Integer

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -6,7 +6,12 @@ defmodule FizzBuzz do
     |> handle_file_read
   end
 
-  def handle_file_read({:ok, content}), do: content
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:ok, content}) do 
+    content
+    |>String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error: #{reason}"
 
 end
